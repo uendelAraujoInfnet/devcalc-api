fb0a8901b155c025c19ee5b92844a6bf71efa407
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-05-04 15:16:15 -0400

    Implementado endpoints da API da calculadora e testes unitários e adicionado o Javalin nas dependencias pom.xml

--- devcalc-api/src/main/java/com/devcalc/App.java
+++ devcalc-api/src/main/java/com/devcalc/App.java
@@ -1,13 +1,37 @@
 package com.devcalc;
 
-/**
- * Hello world!
- *
- */
+import com.devcalc.service.CalculatorService;
+import io.javalin.Javalin;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        CalculatorService service = new CalculatorService();
+
+        Javalin app = Javalin.create().start(7000);
+
+        app.get("/add", ctx -> {
+            double a = Double.parseDouble(ctx.queryParam("a"));
+            double b = Double.parseDouble(ctx.queryParam("b"));
+            ctx.result(String.valueOf(service.add(a, b)));
+        });
+
+        app.get("/subtract", ctx -> {
+            double a = Double.parseDouble(ctx.queryParam("a"));
+            double b = Double.parseDouble(ctx.queryParam("b"));
+            ctx.result(String.valueOf(service.subtract(a , b)));
+        });
+
+        app.get("/multiply", ctx -> {
+            double a = Double.parseDouble(ctx.queryParam("a"));
+            double b = Double.parseDouble(ctx.queryParam("b"));
+            ctx.result(String.valueOf(service.multiply(a, b)));
+        });
+
+        app.get("/divide", ctx -> {
+            double a = Double.parseDouble(ctx.queryParam("a"));
+            double b = Double.parseDouble(ctx.queryParam("b"));
+            ctx.result(String.valueOf(service.divide(a, b)));
+        });
     }
 }
---
+++ devcalc-api/src/main/java/com/devcalc/service/CalculatorService.java
@@ -0,0 +1,23 @@
+package com.devcalc.service;
+
+import com.devcalc.exception.DivisionByZeroException;
+
+public class CalculatorService {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) throws DivisionByZeroException {
+        if(b == 0 ) throw new DivisionByZeroException("O divisor não pode ser zero");
+        return a / b;
+    }
+}
