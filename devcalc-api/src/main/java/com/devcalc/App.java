54cd22e7e447c81457130f2d4d5919f9e285e1b1
Uendel Ives de Araujo <uendel.araujo@al.infnet.edu.br>
2025-05-05 12:24:06 -0400

    Adicionado comentarios para informações no App.java

--- devcalc-api/src/main/java/com/devcalc/App.java
+++ devcalc-api/src/main/java/com/devcalc/App.java
@@ -10,24 +10,28 @@ public class App
 
         Javalin app = Javalin.create().start(7000);
 
+        //Método para chamar o endpoint da adição e já fornecer os valores como parametros
         app.get("/add", ctx -> {
             double a = Double.parseDouble(ctx.queryParam("a"));
             double b = Double.parseDouble(ctx.queryParam("b"));
             ctx.result(String.valueOf(service.add(a, b)));
         });
 
+        //Método para chamar o endpoint da subtração e já fornecer os valores como parametros
         app.get("/subtract", ctx -> {
             double a = Double.parseDouble(ctx.queryParam("a"));
             double b = Double.parseDouble(ctx.queryParam("b"));
             ctx.result(String.valueOf(service.subtract(a , b)));
         });
 
+        //Método para chamar o endpoint da Multiplicação e já fornecer os valores como parametros
         app.get("/multiply", ctx -> {
             double a = Double.parseDouble(ctx.queryParam("a"));
             double b = Double.parseDouble(ctx.queryParam("b"));
             ctx.result(String.valueOf(service.multiply(a, b)));
         });
 
+        //Método para chamar o endpoint da Divisão e já fornecer os valores como parametros
         app.get("/divide", ctx -> {
             double a = Double.parseDouble(ctx.queryParam("a"));
             double b = Double.parseDouble(ctx.queryParam("b"));
